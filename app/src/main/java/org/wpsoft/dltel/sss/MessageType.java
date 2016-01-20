da0840ff4dabd8d5e2abd82bb264fac9613d1814
WinUP <winupsoftware@gmail.com>
2016-01-20 21:02:23 +0800

    架构修改(1/2)
    同时添加了SpellSkillSystem的一部分

---
+++ app/src/main/java/org/wpsoft/dltel/sss/MessageType.java
@@ -0,0 +1,12 @@
+package org.wpsoft.dltel.sss;
+
+/**
+ * SpellSkillSystem - 消息类型
+ * Created by WinUP on 2016/1/20.
+ */
+public enum MessageType {
+    SystemMessage,
+    SpellSkillSystemMessage,
+    MapMessage,
+    CustomizedMessage
+}
