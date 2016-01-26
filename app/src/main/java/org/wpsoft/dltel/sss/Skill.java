5c3ca10dc0b2d19aec5cf5f0b5a28b7b4e73d49e
WinUP <winupsoftware@gmail.com>
2016-01-26 14:40:54 +0800

    Merge branch 'master' of github.com:lsmtty/DeusLegem
    Conflicts:
    	app/src/main/java/org/wpsoft/dltel/sss/Skill.java

---
+++ app/src/main/java/org/wpsoft/dltel/sss/Skill.java
@@ -0,0 +1,25 @@
+package org.wpsoft.dltel.sss;
+
+import org.wpsoft.dltel.system.Card;
+
+/**
+ * 表示一个技能
+ * Created by WinUP on 2016/1/20.
+ */
+public abstract class Skill extends Listener {
+    private Card card;
+
+    public Card getCard() {
+        return card;
+    }
+
+    public Skill(int listenerType, Card card) {
+        super(listenerType);
+        this.card = card;
+        ListenerList.add(this);
+    }
+    public void cancel()
+    {
+
+    }
+}
